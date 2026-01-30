d54cdba111f1c7a99c6d267ec14d959d9b33863d
anshul-999 <anshul.b100@gmail.com>
2026-01-30 16:22:48 +0530

    fix naming in env variables

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -6,17 +6,9 @@ let credential;
 
 if (process.env.NODE_ENV === "production") {
   credential = admin.credential.cert({
-    FIREBASE_type:process.env.FIREBASE_type,
-    FIREBASE_project_id:process.env.FIREBASE_project_id,
-    FIREBASE_private_key_id:process.env.FIREBASE_private_key_id,
-    FIREBASE_private_key:process.env.FIREBASE_private_key.replace(/\\n/g, "\n"),
-    FIREBASE_client_email:process.env.FIREBASE_client_email,
-    FIREBASE_client_id:process.env.FIREBASE_client_id,
-    FIREBASE_auth_uri:process.env.FIREBASE_auth_uri,
-    FIREBASE_token_uri:process.env.FIREBASE_token_uri,
-    FIREBASE_auth_provider_x509_cert_url:process.env.FIREBASE_auth_provider_x509_cert_url,
-    FIREBASE_client_x509_cert_url:process.env.FIREBASE_client_x509_cert_url,
-    FIREBASE_universe_domain:process.env.FIREBASE_universe_domain,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   });
 } else {
   credential = admin.credential.cert(
